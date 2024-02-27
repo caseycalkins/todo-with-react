5ffe9a86cce83eb205ec5f643fece441598d9570
Casey Calkins <a.caseycalkins@outlook.com>
2024-02-27 09:53:16 -0600

    Basic Delete and Complete button finished, need to fix functionality

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -5,6 +5,7 @@ import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import './TaskList.css';
+import DeleteButton from '../DeleteButton/DeleteButton';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -18,19 +19,22 @@ const Item = styled(Paper)(({ theme }) => ({
 function BasicStack({ tasks, onSubmitTask }) {
   return (
     <>
-      <Box sx={{ width: '100%' }}>
+      <Box sx={{ width: '60%' }}>
         <Stack spacing={2}>
           <div>
             {' '}
             {/* Added a <div> as a block-level container */}
-            <ul>
+            <ul className='unordered-list-wrapper'>
               {tasks.map((task, index) => (
                 <li key={index}>
-                  <Item>
+                  <Item className='task-list-section-wrapper'>
                     <Typography variant='h6'>{task.name}</Typography>
                     <Typography variant='body1'>{task.description}</Typography>
                     <Typography variant='body2'>{task.dueDate}</Typography>
                   </Item>
+                    <DeleteButton
+                        onDeleteTask={() => onSubmitTask(index)}
+                    />
                 </li>
               ))}
             </ul>
